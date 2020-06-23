110463b7dc7d7ea11bf4f48df606216363ecf08a
amnguyen2 <abramnguyen210@gmail.com>
2020-06-23 17:21:35 -0600

    completed basic UI

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -3,9 +3,11 @@
 #include "tokenizer.h"
 
 int main() {
-  
-  printf("Enter a string.\n <");
-    
+  char str[200];
+  printf("> Enter a word.\n");
+  printf("< ");
+  fgets(str, sizeof(str), stdin);
+  printf("\n> You entered: %s\n", str);
   
   return 0;
 }
